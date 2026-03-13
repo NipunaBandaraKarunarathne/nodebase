40d60c7e8dcc808f6cab2b7ebcfc8e6e22ef6368
Nipuna Bandara Karunarathne <66937343+NipunaBandaraKarunarathne@users.noreply.github.com>
2026-03-14 02:50:34 +0530

    db integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
-import Image from "next/image";
+""
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
 
-export default function Home() {
+const Home = async () => {
+  const users = await prisma.user.findMany();
+
   return (
     <div className="text-red-500">
- Hello world
- <div className=" min-h-screen min-w-screen flex items-center justify-center">
-      <Button variant="outline">click me</Button>
- </div>
+      Hello world
+      <pre>{JSON.stringify(users, null, 2)}</pre>
+
+      <div className="min-h-screen min-w-screen flex items-center justify-center">
+        <Button variant="outline">click me</Button>
+      </div>
     </div>
   );
-}
+};
+
+export default Home;
