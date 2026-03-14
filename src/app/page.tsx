40eb36ef6863d6104d277bd25f6e2e91136e45d3
Nipuna Bandara Karunarathne <66937343+NipunaBandaraKarunarathne@users.noreply.github.com>
2026-03-15 00:11:28 +0530

    adding suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { caller, trpc } from "@/trpc/server";
 import ClientComponent from "./client";
 import { getQueryClient } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
 
 const Home = async () => {
   const queryClient = getQueryClient();
@@ -20,7 +21,10 @@ const Home = async () => {
       <div className="min-h-screen min-w-screen flex items-center justify-center">
         <Button variant="outline">click me</Button>
         <HydrationBoundary state={dehydrate(queryClient)}>
-          <ClientComponent  />
+          <Suspense fallback={<div>Loading...</div>}>
+            <ClientComponent  />
+          </Suspense>
+          
         </HydrationBoundary>
         
       </div>
