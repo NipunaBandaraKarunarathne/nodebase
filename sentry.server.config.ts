64a4a9b7e68913b883d17e4af132bcb1db1b4a45
Nipuna Bandara Karunarathne <66937343+NipunaBandaraKarunarathne@users.noreply.github.com>
2026-03-20 00:42:22 +0530

    sentry integrated

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -6,14 +6,9 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://3f83cfa2f51d5b94489a0ae3586eabe2@o4511072191119360.ingest.us.sentry.io/4511072220676096",
-
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
-
-  // Enable logs to be sent to Sentry
-  enableLogs: true,
-
-  // Enable sending user PII (Personally Identifiable Information)
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  // Tracing must be enabled for agent monitoring to work
+  tracesSampleRate: 1.0,
+  // Add data like inputs and responses to/from LLMs and tools;
+  // see https://docs.sentry.io/platforms/javascript/data-management/data-collected/ for more info
   sendDefaultPii: true,
 });
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -3,17 +3,21 @@ import { inngest } from "./client";
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { generateText } from "ai";
 
-const google= createGoogleGenerativeAI();
+const google = createGoogleGenerativeAI();
 
 export const executeAI = inngest.createFunction(
   { id: "execute-ai", triggers: [{ event: "execute/ai" }] },
   async ({ event, step }) => {
-
-    const {steps} = await step.ai.wrap("gemini-generate-text", generateText,{
-        model: google("gemini-2.5-flash"),
-        system: "You are a helpful assistant for generating text",
-        prompt:"what is 2+2?",
-    })
-return steps;
+    const { steps } = await step.ai.wrap("gemini-generate-text", generateText, {
+      model: google("gemini-2.5-flash"),
+      system: "You are a helpful assistant for generating text",
+      prompt: "what is 2+2?",
+      experimental_telemetry: {
+        isEnabled: true,
+        recordInputs: true,
+        recordOutputs: true,
+      },
+    });
+    return steps;
   },
 );
