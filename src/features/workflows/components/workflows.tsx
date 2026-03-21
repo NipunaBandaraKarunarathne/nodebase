19ac8bef802372e4bfc1a5de2830e12504c4867b
Nipuna Bandara Karunarathne <66937343+NipunaBandaraKarunarathne@users.noreply.github.com>
2026-03-22 00:35:56 +0530

    workflow header added

--- src/features/workflows/components/workflows.tsx
+++ src/features/workflows/components/workflows.tsx
@@ -1,11 +1,42 @@
 "use client";
 import { json } from "zod";
-import { useSuspenseWorkflows } from "../hooks/use-workflows"
+import { useSuspenseWorkflows } from "../hooks/use-workflows";
+import { EntityContainer, EntityHeader } from "@/components/entity-components";
+import React from "react";
 
-export const WorkflowsList=()=>{
-    const workflows = useSuspenseWorkflows();
+export const WorkflowsList = () => {
+  const workflows = useSuspenseWorkflows();
 
-    return(
-        <p>{JSON.stringify(workflows.data,null,2)}</p>
-    )
-}
+  return <p>{JSON.stringify(workflows.data, null, 2)}</p>;
+};
+
+export const WorkflowsHeader = ({ disabled }: { disabled?: boolean }) => {
+  return (
+    <>
+      <EntityHeader
+        title="workflows"
+        description="Create and Manage your Workflows"
+        onNew={() => {}}
+        newButtonLable="New workflow"
+        disabled={disabled}
+        isCreating={false}
+      />
+    </>
+  );
+};
+
+export const WorkFlowContainer = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <EntityContainer
+      header={<WorkflowsHeader />}
+      search={<></>}
+      pagination={<></>}
+    >
+      {children}
+    </EntityContainer>
+  );
+};
