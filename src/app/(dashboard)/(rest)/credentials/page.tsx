1a8ad0f4ffc605be7fd5cff6d8329b2187e4a3e7
Nipuna Bandara Karunarathne <66937343+NipunaBandaraKarunarathne@users.noreply.github.com>
2026-04-02 01:28:35 +0530

    credentials

--- src/app/(dashboard)/(rest)/credentials/page.tsx
+++ src/app/(dashboard)/(rest)/credentials/page.tsx
@@ -1,7 +1,28 @@
+import { credentialsParamsLoader } from "@/features/credentials/server/params-loader";
+import { prefetchCredentials } from "@/features/credentials/server/prefetch";
 import { requireAuth } from "@/lib/auth-utils";
+import { HydrateClient } from "@/trpc/server";
+import { ErrorBoundary } from "react-error-boundary";
+import { SearchParams } from "nuqs";
+import { Suspense } from "react";
+import { CredentialsList } from "@/features/credentials/components/credentials";
 
-const Page = async() => {
-    await requireAuth(); 
-    return <div>Credentials</div>
-}
-export default Page 
+type Props = {
+  searchParams: Promise<SearchParams>;
+};
+
+const Page = async ({ searchParams }: Props) => {
+  await requireAuth();
+  const params = await credentialsParamsLoader(searchParams);
+  prefetchCredentials(params);
+  return (
+    <HydrateClient>
+      <ErrorBoundary fallback={<div>Failed to load credentials</div>}>
+        <Suspense fallback={<div>Loading credentials...</div>}>
+          <CredentialsList/>
+        </Suspense>
+      </ErrorBoundary>
+    </HydrateClient>
+  );
+};
+export default Page;
